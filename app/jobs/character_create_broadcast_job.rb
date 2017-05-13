8898661c10bd5379b814a31721e711e89051bf31
Isaac Priestley <progressions@gmail.com>
2017-05-13 14:16:50 -0500

    Trying to add create action to characters channel

---
+++ app/jobs/character_create_broadcast_job.rb
@@ -0,0 +1,14 @@
+class CharacterCreateBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(data)
+    character = Character.find(data['id'])
+    ActionCable.server.broadcast "characters_channel", id: character.id, data: render_character(character)
+  end
+
+  private
+
+    def render_character(character)
+      ApplicationController.renderer.render(partial: 'characters/character', locals: {character: character})
+    end
+end

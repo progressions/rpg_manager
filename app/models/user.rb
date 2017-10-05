8b1dc24d6688cbf4785b706159112a3077434488
Isaac Priestley <progressions@gmail.com>
2017-10-05 10:48:53 -0500

    Use more general name for RPG Manager, to make it usable in any RPG

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 
   has_many :party_memberships
   has_many :encounters, through: :party_memberships
